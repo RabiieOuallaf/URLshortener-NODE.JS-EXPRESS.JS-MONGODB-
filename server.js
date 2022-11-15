d9280381126e2e298c508b1ec6f67148a8f2fe1e
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-11-15 19:22:55 +0100

    Setting up mongoose

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const shortUrl = require("./modules/urlShortner");
 const app = express();
 
+mongoose.connect("mongodb://localhost/urlShortner")
 const PORT = process.env.PORT || 5000;
 
+app.set("view engine" , "ejs");
+
+app.get('/' , (req, res) => {
+
+    res.render("index");
+
+})
+
+app.get('/shortUrls', )
 
 
 app.listen(PORT, () => {
